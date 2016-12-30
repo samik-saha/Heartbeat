facd81812f25253151b7f68a9db952992ed27575
Samik Saha <samiksaha88@gmail.com>
2016-12-31 00:44:41 +0530

    update

---
+++ app/src/main/java/io/github/samik_saha/heartbeat/EmailPasswordActivity.java
@@ -0,0 +1,15 @@
+package io.github.samik_saha.heartbeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmailPasswordActivity extends AppCompatActivity {
+
+    private final static String TAG="EmailPassword";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_password);
+    }
+}
